68a8864e33a351e6955df929a1df4c21e71ad752
sebug <sebastian.gfeller@gmail.com>
2021-11-13 12:08:54 +0100

    Setting alive dead

--- session3/GameOfLife/Cell.cs
+++ session3/GameOfLife/Cell.cs
@@ -3,10 +3,20 @@ namespace GameOfLife
 {
     public class Cell
     {
+        public Cell(bool isAlive)
+        {
+            this.isAlive = isAlive;
+        }
+
         private bool isAlive = false;
         internal bool IsDead()
         {
             return !isAlive;
+        }
+
+        internal bool IsAlive()
+        {
+            return isAlive;
         }
     }
 }
--- session3/GameOfLife/Program.cs
+++ session3/GameOfLife/Program.cs
@@ -8,12 +8,19 @@ namespace GameOfLife
         static void Main(string[] args)
         {
             ACellCanBeDead();
+            ACellCanBeAlive();
         }
 
         private static void ACellCanBeDead()
         {
-            var cell = new Cell();
+            var cell = new Cell(false);
             Debug.Assert(cell.IsDead());
+        }
+
+        private static void ACellCanBeAlive()
+        {
+            var cell = new Cell(true);
+            Debug.Assert(cell.IsAlive());
         }
     }
 }
